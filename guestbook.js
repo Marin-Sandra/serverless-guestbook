b23fcec5539a8fab5ae8f04eed5bcb9de524c16b
Marin-Sandra <108886515+Marin-Sandra@users.noreply.github.com>
2022-07-23 19:52:35 +0530

    commit changes

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://79a1cb90.us-south.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
